c9c241cae2593efafd8f140a2bcd7dbe21900543
vulezor@gmail.com <vulezor@gmail.com>
2016-09-18 14:17:42 +0200

    Update changes

--- controllers/test_api.php
+++ controllers/test_api.php
@@ -3,30 +3,23 @@ class Test_Api extends Controller{
 	public function __construct()
     {
         parent::__construct();
-       //Ajax::ajaxCheck();
-       
-	   $check_session = $this->check_logedIn($data->session_id); //checking if session exists
-	    if(!$check_session){
-	    	echo json_encode(array('logedIn'=>0), JSON_NUMERIC_CHECK);
-	    	die();
-	    }
-    }
-
-    private function check_logedIn($session_id)
-    {
-        Session::set_session_id($session_id);
-        Session::init();
+		if(isset($_GET['session_id'])){
+			Session::set_session_id($session_id);
+		}
+       	Session::init();
         $logged = Session::get('loggedIn');
         $status = Session::get('role');
-        if ($logged == false && $status != 'magacioner') {
+        if ($logged == false || $status != 'Administrator' && $status != 'Redovan korisnik' && $status != 'Logistika') {
             unset($logged);
             unset($status);
             Session::destroy();
-            return false;
-        } else {
-            return true;
-        }
+            header('Content-Type: application/json');
+            echo json_encode(array('logedIn'=>0), JSON_NUMERIC_CHECK);
+            die;
+        } 
+	
     }
+
 
     public function check(){
     	echo "only check";
